d2fb06e88a57b296e7fc13673602933e5ebcd663
Way Yan <wayyan.win@gmail.com>
2026-08-14 16:30:33 +0800

    rename chatbot and change welcome msg

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-# Duke User Guide
+# Puke User Guide
 
 // Update the title above to match the actual product name
 
---
+++ src/main/java/Puke.java
@@ -0,0 +1,14 @@
+public class Puke {
+    public static void main(String[] args) {
+        String banner = "██████╗ ██╗   ██╗██╗  ██╗███████╗\n"
+                      + "██╔══██╗██║   ██║██║ ██╔╝██╔════╝\n"
+                      + "██████╔╝██║   ██║█████╔╝ █████╗  \n"
+                      + "██╔═══╝ ██║   ██║██╔═██╗ ██╔══╝  \n"
+                      + "██║     ╚██████╔╝██║  ██╗███████╗\n"
+                      + "╚═╝      ╚═════╝ ╚═╝  ╚═╝╚══════╝";
+        System.out.println(banner);
+        System.out.println("hello i'm puke ask me anyth bro");
+        System.out.println("-------------------------------");
+        System.out.println("nvm i'm dipping lol bye");
+    }
+}
